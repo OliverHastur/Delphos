004e09a7bddcae77af873b3910a1737f33bbabeb
OliverHastur <olivermariosantos15@gmail.com>
2026-05-13 14:55:20 -0300

    Atualização Schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,18 +1,18 @@
--- TABELA PRINCIPAL: Os Códices (Baseada no seu CSV)
+-- Tabela Principal 
 CREATE TABLE livros (
     id SERIAL PRIMARY KEY,
     autor VARCHAR(255),
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     isbn VARCHAR(50),
-    nota INTEGER, -- O Julgamento (1 a 5)
+    nota INTEGER, -- (1 a 5)
     numero_paginas INTEGER,
     preco_pago DECIMAL(10,2),
-    status VARCHAR(50), -- LIDO, LENDO, QUERO LER (A UI traduz para o Fio das Moiras)
+    status VARCHAR(50), -- LIDO, LENDO, QUERO LER
     titulo VARCHAR(255) NOT NULL,
     url_capa TEXT
 );
 
--- TABELA DE APOIO: Os Pergaminhos (Notas Markdown)
+-- Tabela de Apoio - Anotações
 CREATE TABLE pergaminhos_anotacoes (
     id SERIAL PRIMARY KEY,
     livro_id INTEGER REFERENCES livros(id) ON DELETE CASCADE,
@@ -20,7 +20,7 @@ CREATE TABLE pergaminhos_anotacoes (
     data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- TABELA DE APOIO: O Panteão (Personagens)
+-- Tabela de Apoio - Personagens
 CREATE TABLE panteao_personagens (
     id SERIAL PRIMARY KEY,
     livro_id INTEGER REFERENCES livros(id) ON DELETE CASCADE,
